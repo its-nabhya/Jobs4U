4222b63be3dca6dc5dcf4002ab3d5cf70199d20f
Nabhya Parsramka <140908408+its-nabhya@users.noreply.github.com>
2025-08-24 13:13:53 +0530

    Initial commit, set up and .gitignore

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# route basically provides the URL or path after the domain 
+@app.route("/") # Empty route- basically home page 
+# by doing this we ask flask to execute hello_world() when  url is domain/  i.e. our landing page
+def hello_world():
+    html="""<div style="color:red;">First code up and running
+    <p>Hello, World!</p>
+    </div>"""
+    return render_template('home.html')
+
+
+print(__name__)
+if __name__ =="__main__":
+    app.run(host='0.0.0.0', debug=True)
